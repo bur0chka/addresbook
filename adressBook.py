21ef3a3e82544b3d2f2d9ee0e29f0433b34ae606
bur0chka <burashd124@gmail.com>
2021-08-07 15:25:07 +0500

    patch 0.1

--- adressBook.py
+++ adressBook.py
@@ -1,3 +1,25 @@
 from tkinter import *
 
 addresBook = Tk()
+addresBook.title('Адресная книга')
+addresBook.geometry('400x500')
+
+def inputcontact():
+    inputTitle.place(x=40, y=100)
+    inputNumber.place(x=80, y=200)
+    cancel.place(x=134, y=350)
+    addcontact.place(x=200, y=350)
+    add.pack_forget()
+
+
+def cancelAdd():
+    add.pack()
+
+inputTitle = Text(width=40, height=2)
+inputNumber = Text(width=30, height=1)
+add = Button(addresBook, text='Добавить контакт', width=15, height=2, command=inputcontact)
+add.place(x=145, y=0)
+add.pack()
+cancel = Button(addresBook, text='отмена', width=8, height=2, command=cancelAdd)
+addcontact = Button(addresBook, text='добавить', width=8, height=2)
+addresBook.mainloop()
