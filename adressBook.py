b1a2c42825d631b55a21ef4ece4b18409c3cdb94
bur0chka <burashd124@gmail.com>
2021-08-09 16:24:37 +0500

    patch 0.5
    Added 'delete' and 'change' buttons.
    When you add contacts again, the input fields are now empty.
    Contacts are added differently now

--- adressBook.py
+++ adressBook.py
@@ -9,6 +9,8 @@ def inputcontact():
     contactTitle.pack_forget()
     inputTitle.place(x=40, y=100)
     inputNumber.place(x=80, y=200)
+    inputTitle.delete(1.0, END)
+    inputNumber.delete(1.0, END)
     textinputTitle.place(x=125, y=70)
     textinputNumber.place(x=130, y=170)
     cancel.place(x=134, y=350)
@@ -32,11 +34,21 @@ def cancelAdd():
     textinputNumber.pack_forget()
 
 
+def deletecontact():
+    for i in titles:
+        titles.remove(i)
+    for i in numbers:
+        numbers.remove(i)
+
+
 def savecontact():
     global heightTitle
     global heightNumber
+    global heightDeleteEdit
     saveTitle = inputTitle.get('1.0', 'end-1c')
     saveNumber = inputNumber.get('1.0', 'end-1c')
+    titles.append(saveTitle)
+    numbers.append(saveNumber)
     add.pack()
     cancel.pack()
     cancel.pack_forget()
@@ -50,24 +62,36 @@ def savecontact():
     textinputTitle.pack_forget()
     textinputNumber.pack()
     textinputNumber.pack_forget()
-    contactTitle = Label(text=saveTitle, font='25')
-    heightTitle += 43
-    contactTitle.place(x=30, y=heightTitle)
-    contactNumber = Label(text=saveNumber, font='25')
-    heightNumber += 44
-    contactNumber.place(x=30, y=heightNumber)
+    for i in titles:
+        contactTitle = Label(text=i, font='25')
+        heightTitle += 55
+        contactTitle.place(x=30, y=heightTitle)
+    for i in numbers:
+        contactNumber = Label(text=i, font='25')
+        contactNumber.place(x=30, y=heightNumber)
+        heightNumber += 85
+        heightDeleteEdit += 25
+        delete.place(x=325, y=heightDeleteEdit)
+        edit.place(x=259, y=heightDeleteEdit)
 
 
 heightNumber = 25
-heightTitle = 50
+heightTitle = 40
+heightDeleteEdit = 70
+titles = []
+numbers = []
 textinputTitle = Label(text='Введите название:', font='25')
 textinputNumber = Label(text='Введите номер:', font='25')
 inputTitle = Text(width=40, height=2)
 inputNumber = Text(width=30, height=1)
-save = inputTitle.get('1.0', 'end-1c')
-contactTitle = Label(text=save, font='25')
+saveTitle = inputTitle.get('1.0', 'end-1c')
+saveNumber = inputNumber.get('1.0', 'end-1c')
+contactTitle = Label(text=saveTitle, font='25')
+contactNumber = Label(text=saveNumber, font='25')
 add = Button(addresBook, text='Добавить контакт', width=15, height=2, command=inputcontact)
 add.pack()
+delete = Button(addresBook, text='удалить', width=8, height=2, command=deletecontact)
 cancel = Button(addresBook, text='отмена', width=8, height=2, command=cancelAdd)
 addcontact = Button(addresBook, text='добавить', width=8, height=2, command=savecontact)
+edit = Button(addresBook, text='именить', width=8, height=2)
 addresBook.mainloop()
