b11f42f92078473def37a378f88fbbd426ff09e9
bur0chka <burashd124@gmail.com>
2021-08-10 19:17:51 +0500

    patch 0.65
    The 'delete' and 'edit' buttons now appear for every added contact. Increased window resolution. The 'delete' button now correctly deletes a contact

--- adressBook.py
+++ adressBook.py
@@ -2,19 +2,18 @@ from tkinter import *
 
 addresBook = Tk()
 addresBook.title('Адресная книга')
-addresBook.geometry('400x500')
+addresBook.geometry('800x600')
 
 def inputcontact():
-    contactTitle.pack()
     contactTitle.pack_forget()
-    inputTitle.place(x=40, y=100)
-    inputNumber.place(x=80, y=200)
+    inputTitle.place(x=460, y=100)
+    inputNumber.place(x=520, y=200)
     inputTitle.delete(1.0, END)
     inputNumber.delete(1.0, END)
-    textinputTitle.place(x=125, y=70)
-    textinputNumber.place(x=130, y=170)
-    cancel.place(x=134, y=350)
-    addcontact.place(x=200, y=350)
+    textinputTitle.place(x=370, y=70)
+    textinputNumber.place(x=375, y=170)
+    cancel.place(x=500, y=350)
+    addcontact.place(x=566, y=350)
     add.pack_forget()
 
 
@@ -34,49 +33,61 @@ def cancelAdd():
     textinputNumber.pack_forget()
 
 
-def deletecontact():
-    for i in titles:
-        titles.remove(i)
-    for i in numbers:
-        numbers.remove(i)
+#def deletecontact():
+
 
 
 def savecontact():
+    def delete():
+        contactTitle.destroy()
+        contactNumber.destroy()
+        deleteb.destroy()
+        changeb.destroy()
+
+    def change():
+        changetk = Tk()
+        changetk.title("Изменить")
+        changetk.geometry("400x500")
+        editTitle = Text(width=40, height=2, master=changetk)
+        editNumber = Text(width=30, height=1, master=changetk)
+        editTitle.place(x=39, y=100)
+        editNumber.place(x=79, y=200)
+        editcontact = Button(changetk, text='Изменить контакт', width=15, height=2)
+        editcontact.place(x=150, y=300)
+
+    global score
     global heightTitle
     global heightNumber
     global heightDeleteEdit
+    score += 1
     saveTitle = inputTitle.get('1.0', 'end-1c')
     saveNumber = inputNumber.get('1.0', 'end-1c')
-    titles.append(saveTitle)
-    numbers.append(saveNumber)
-    add.pack()
-    cancel.pack()
-    cancel.pack_forget()
-    addcontact.pack()
-    addcontact.pack_forget()
-    inputNumber.pack()
-    inputNumber.pack_forget()
-    inputTitle.pack()
-    inputTitle.pack_forget()
-    textinputTitle.pack()
-    textinputTitle.pack_forget()
-    textinputNumber.pack()
-    textinputNumber.pack_forget()
-    for i in titles:
-        contactTitle = Label(text=i, font='25')
-        heightTitle += 55
-        contactTitle.place(x=30, y=heightTitle)
-    for i in numbers:
-        contactNumber = Label(text=i, font='25')
-        contactNumber.place(x=30, y=heightNumber)
-        heightNumber += 85
-        heightDeleteEdit += 25
-        delete.place(x=325, y=heightDeleteEdit)
-        edit.place(x=259, y=heightDeleteEdit)
+    #titles.append(saveTitle)
+    #numbers.append(saveNumber)
+    #for i in titles:
+    contactTitle = Label(text=saveTitle, font='25')
+    heightTitle += 15
+    changeb = Button(text="Изменить", command=change)
+    deleteb = Button(text="Удалить", command=delete)
+    print(heightTitle)
+    contactTitle.grid(row=score, column=0)
+    changeb.grid(row=score, column=2)
+    deleteb.grid(row=score, column=3)
+    #for i in numbers:
+    contactNumber = Label(text=saveNumber, font='25')
+    contactNumber.grid(row=score, column=1)
+    heightNumber += 15
+    print(heightNumber)
+    heightDeleteEdit += 25
 
 
-heightNumber = 25
-heightTitle = 40
+
+
+
+
+score  = 1 # счетчик контактов
+heightNumber = 45
+heightTitle = 55
 heightDeleteEdit = 70
 titles = []
 numbers = []
@@ -90,8 +101,6 @@ contactTitle = Label(text=saveTitle, font='25')
 contactNumber = Label(text=saveNumber, font='25')
 add = Button(addresBook, text='Добавить контакт', width=15, height=2, command=inputcontact)
 add.pack()
-delete = Button(addresBook, text='удалить', width=8, height=2, command=deletecontact)
 cancel = Button(addresBook, text='отмена', width=8, height=2, command=cancelAdd)
 addcontact = Button(addresBook, text='добавить', width=8, height=2, command=savecontact)
-edit = Button(addresBook, text='именить', width=8, height=2)
 addresBook.mainloop()
