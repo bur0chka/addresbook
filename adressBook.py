6808cddc3db76ab6c2480b35f5dfb0fbd5e34969
bur0chka <burashd124@gmail.com>
2021-08-11 20:30:56 +0500

    patch 0.75
    The 'Edit' button now works. Now you can not add empty lines to contacts

--- adressBook.py
+++ adressBook.py
@@ -1,10 +1,8 @@
 from tkinter import *
 
-addresBook = Tk()
-addresBook.title('Адресная книга')
-addresBook.geometry('800x600')
 
 def inputcontact():
+    """ Ввод названия и номера контакта"""
     contactTitle.pack_forget()
     inputTitle.place(x=460, y=100)
     inputNumber.place(x=520, y=200)
@@ -18,6 +16,7 @@ def inputcontact():
 
 
 def cancelAdd():
+    """Отмена добавления контакта"""
     add.pack()
     cancel.pack()
     cancel.pack_forget()
@@ -33,18 +32,27 @@ def cancelAdd():
     textinputNumber.pack_forget()
 
 
-#def deletecontact():
-
-
-
-def savecontact():
-    def delete():
+def savecontact(): # Создание контакта
+    def delete(): # Удаление контакта
         contactTitle.destroy()
         contactNumber.destroy()
         deleteb.destroy()
         changeb.destroy()
 
-    def change():
+    def change():  # Изменение контакта
+        def changecontact():
+            saveNumber = editTitle.get('1.0', 'end-1c')
+            saveTitle = editNumber.get('1.0', 'end-1c')
+            if saveNumber != '' and saveTitle != '':
+                contactTitle.destroy()
+                contactNumber.destroy()
+                contactTitle_edit = Label(text=saveTitle, font='25', master=addresBook)  # Добавление контакта
+                contactNumber_edit = Label(text=saveNumber, font='25', master=addresBook)
+                contactTitle_edit.grid(row=score, column=0)
+                contactNumber_edit.grid(row=score, column=1)
+                changetk.destroy()
+                print(editTitle)
+                print(editNumber)
         changetk = Tk()
         changetk.title("Изменить")
         changetk.geometry("400x500")
@@ -52,8 +60,13 @@ def savecontact():
         editNumber = Text(width=30, height=1, master=changetk)
         editTitle.place(x=39, y=100)
         editNumber.place(x=79, y=200)
-        editcontact = Button(changetk, text='Изменить контакт', width=15, height=2)
+
+
+        editcontact = Button(changetk, text='Изменить контакт', width=15, height=2, command=changecontact)
         editcontact.place(x=150, y=300)
+        # кнопка отмена
+        cancel_change = Button(changetk, text='Отмена', width=15, height=2, command=changetk.destroy)
+        cancel_change.place(x=150, y=415)
 
     global score
     global heightTitle
@@ -62,45 +75,43 @@ def savecontact():
     score += 1
     saveTitle = inputTitle.get('1.0', 'end-1c')
     saveNumber = inputNumber.get('1.0', 'end-1c')
-    #titles.append(saveTitle)
-    #numbers.append(saveNumber)
-    #for i in titles:
+    if saveNumber != '' and saveTitle != '':
+        contactTitle = Label(text=saveTitle, font='25')  # Добавление контакта
+        changeb = Button(text="Изменить", command=change)
+        deleteb = Button(text="Удалить", command=delete)
+        print(heightTitle)
+        contactTitle.grid(row=score, column=0)
+        changeb.grid(row=score, column=2)
+        deleteb.grid(row=score, column=3)
+        #for i in numbers:
+        contactNumber = Label(text=saveNumber, font='25')
+        contactNumber.grid(row=score, column=1)
+        print(heightNumber)
+
+
+
+
+if __name__ == '__main__':
+    addresBook = Tk()
+    addresBook.title('Адресная книга')
+    addresBook.geometry('800x600')
+
+    score  = 1 # счетчик контактов
+    heightNumber = 45
+    heightTitle = 55
+    heightDeleteEdit = 70
+    titles = []
+    numbers = []
+    textinputTitle = Label(text='Введите название:', font='25')
+    textinputNumber = Label(text='Введите номер:', font='25')
+    inputTitle = Text(width=40, height=2)
+    inputNumber = Text(width=30, height=1)
+    saveTitle = inputTitle.get('1.0', 'end-1c')
+    saveNumber = inputNumber.get('1.0', 'end-1c')
     contactTitle = Label(text=saveTitle, font='25')
-    heightTitle += 15
-    changeb = Button(text="Изменить", command=change)
-    deleteb = Button(text="Удалить", command=delete)
-    print(heightTitle)
-    contactTitle.grid(row=score, column=0)
-    changeb.grid(row=score, column=2)
-    deleteb.grid(row=score, column=3)
-    #for i in numbers:
     contactNumber = Label(text=saveNumber, font='25')
-    contactNumber.grid(row=score, column=1)
-    heightNumber += 15
-    print(heightNumber)
-    heightDeleteEdit += 25
-
-
-
-
-
-
-score  = 1 # счетчик контактов
-heightNumber = 45
-heightTitle = 55
-heightDeleteEdit = 70
-titles = []
-numbers = []
-textinputTitle = Label(text='Введите название:', font='25')
-textinputNumber = Label(text='Введите номер:', font='25')
-inputTitle = Text(width=40, height=2)
-inputNumber = Text(width=30, height=1)
-saveTitle = inputTitle.get('1.0', 'end-1c')
-saveNumber = inputNumber.get('1.0', 'end-1c')
-contactTitle = Label(text=saveTitle, font='25')
-contactNumber = Label(text=saveNumber, font='25')
-add = Button(addresBook, text='Добавить контакт', width=15, height=2, command=inputcontact)
-add.pack()
-cancel = Button(addresBook, text='отмена', width=8, height=2, command=cancelAdd)
-addcontact = Button(addresBook, text='добавить', width=8, height=2, command=savecontact)
-addresBook.mainloop()
+    add = Button(addresBook, text='Добавить контакт', width=15, height=2, command=inputcontact)
+    add.pack()
+    cancel = Button(addresBook, text='отмена', width=8, height=2, command=cancelAdd)
+    addcontact = Button(addresBook, text='добавить', width=8, height=2, command=savecontact)
+    addresBook.mainloop()
